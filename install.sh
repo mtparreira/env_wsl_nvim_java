2ec00131a7131b49154a525c433a7b26fdb921f5
Marcos Tiago Parreira <mtparreira@gmail.com>
2025-02-21 15:10:46 -0300

    main

---
+++ install.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+cp nvim/ini.lua $HOME/.config/nvim/.
+cp nvim/lua/community.lua $HOME/.config/nvim/lua/.
+cp nvim/lua/java_setup.lua $HOME/.config/nvim/lua/.
+cp nvim/lua/kanagawa_setup.lua $HOME/.config/nvim/lua/.
+cp nvim/lua/lualine_setup.lua $HOME/.config/nvim/lua/.
+cp nvim/lua/options.lua $HOME/.config/nvim/lua/.
+cp nvim/lua/plugins/conform.lua $HOME/.config/nvim/lua/plugins/.
+cp nvim/lua/plugins/java.lua $HOME/.config/nvim/lua/plugins/.
+cp nvim/lua/plugins/kanagawa.lua $HOME/.config/nvim/lua/plugins/.
+cp nvim/lua/plugins/lualine.lua $HOME/.config/nvim/lua/plugins/.
+cp nvim/lua/plugins/options.lua $HOME/.config/nvim/lua/plugins/.
+tar -xzvf demo/demo.tar.gz $HOME/workspace/
