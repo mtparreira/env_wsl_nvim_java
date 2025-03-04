31f9082135e5ad00fbdedd33dfd7d4e3c6cdf61e
Marcos Tiago Parreira <mtparreira@gmail.com>
2025-03-04 14:13:47 -0300

    main

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,17 +1,22 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      html = { "prettier" },
-      css = { "prettier" },
-      json = { "prettier" },
-      xml = { "xmlformatter" }, -- Para arquivos CML (se for XML)
-      markdown = { "prettier" },
-      sql = { "sql-formatter" },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			json = { "prettier" },
+			markdown = { "prettier" },
+			sql = { "sql-formatter" },
+			xml = { "xmlformatter" }, -- Para arquivos CML (se for XML)
+			lua = { "stylua" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
